6317db2016e7c2338a81e007689263e2c7df4a6a
ColeHend <darkgale93@gmail.com>
2023-02-16 20:45:35 -0700

    added login and register routes

---
+++ server/controllers/loginControllers.js
@@ -0,0 +1,5 @@
+const Login = (req, res) => {};
+
+const Register = (req, res) => {};
+
+module.exports = { Login, Register };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
+const { Login, Register } = require("./controllers/loginControllers");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
 app.use(cors());
 
-// Insert Routes Here
+app.post("/login", Login);
+app.post("/register", Register);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
