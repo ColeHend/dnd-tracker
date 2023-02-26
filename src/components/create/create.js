021f0ee9052a51bc28dfcf608897aae35d86c7a0
SladeAnderson <sladeanderson85@gmail.com>
2023-02-25 19:06:47 -0800

    unfinished

--- src/components/create/create.js
+++ src/components/create/create.js
@@ -1,12 +1,27 @@
 // @ts-nocheck
 
+import { Button } from "@mui/material";
 import React, { useContext } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import { UserContext } from "../../App";
 function Create(props) {
  
+  const mySwal = withReactContent(Swal)
+
+
+  function createAProject() {
+    
+  }
+
   return (
-   <>
-   </>
+   <div>
+    <Button
+    sx={{ color: "white" }}
+     onClick={}     
+    >create Project
+    </Button>
+   </div>
   );
 }
 export default Create;
