9878230a8a0c093856910281af905514f8cc5426
SladeAnderson <sladeanderson85@gmail.com>
2023-03-17 20:27:10 -0700

    makeing things look beter

--- src/components/viewProject/viewProject.js
+++ src/components/viewProject/viewProject.js
@@ -9,6 +9,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import './viewProject.scss'
 
 
 function ViewProject(props) {
@@ -28,8 +29,8 @@ function ViewProject(props) {
 
 return (
     <div>
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: '50vw'}}>
+        <TableContainer id='viewProjectTable' component={Paper}>
+            <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
                         <TableCell>Project Name</TableCell>
